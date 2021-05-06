d4ce3e9306dca4bc4c299db72dc9a2f34a108d04
Nathan Skiles <nateskiles94@gmail.com>
2021-05-06 16:21:28 -0700

    MethodsAndObjects Assignment

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -10,26 +10,31 @@ namespace BlackJackGame
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
-            deck.Shuffle();
-
-            foreach  (Card card in deck.Cards )
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
-
+            TwentyOne game = new TwentyOne();
+            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
+            game.ListPlayers();
             Console.ReadLine();
-        }
 
-        // Overloaded method
-        //public static Deck Shuffle (Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
+            //    Deck deck = new Deck();
+            //    deck.Shuffle();
+
+            //    foreach  (Card card in deck.Cards )
+            //    {
+            //        Console.WriteLine(card.Face + " of " + card.Suit);
+            //    }
+            //    Console.WriteLine(deck.Cards.Count);
+
+            //    Console.ReadLine();
+            //}
+
+            // Overloaded method
+            //public static Deck Shuffle (Deck deck, int times)
+            //{
+            //    for (int i = 0; i < times; i++)
+            //    {
+            //        deck = Shuffle(deck);
+            //    }
+            //    return deck;
+        }
     }
 }
