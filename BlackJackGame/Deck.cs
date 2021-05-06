922d0ce63166092c48dbc5cb089bc365f902789f
Nathan Skiles <nateskiles94@gmail.com>
2021-05-06 15:09:04 -0700

    ClassesAssignment

--- BlackJackGame/Deck.cs
+++ BlackJackGame/Deck.cs
@@ -37,5 +37,22 @@ namespace BlackJackGame
 
         public List<Card> Cards { get; set; }
 
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (this.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, this.Cards.Count);
+                    TempList.Add(this.Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                this.Cards = TempList;
+            }
+        }
+
     }
 }
---
+++ ClassAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In the Main() method, instantiate that class. Have the user enter a number. Call the method on that number.
+            Console.WriteLine("Please enter a positive integer: ");
+            try
+            {
+                int dividend = Convert.ToInt32(Console.ReadLine());
+                // Display the output to the screen. It should be the entered number, divided by two.
+                Operation.Divide(dividend);
+                Operation.Multiply();
+                Operation.Multiply(product: dividend);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
