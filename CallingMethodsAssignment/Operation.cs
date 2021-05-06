9c9861208f22326a0732dcddecc978ef644cb08b
Nathan Skiles <nateskiles94@gmail.com>
2021-05-05 17:57:48 -0700

    Calling Methods Assignment

---
+++ CallingMethodsAssignment/Operation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Operation
+    {
+        // Create methods of Operation
+        // Double and return value passed to it
+        public static int DoubleNum(int num)
+        {
+            return num * num;
+        }
+        
+        // Triple number passed
+        public static int TripleNum(int num)
+        {
+            return num * num * num;
+        }
+
+        // Check if number is odd or even
+        public static string OddEven(int num)
+        {
+            string even = num + " is even.";
+            string odd = num + " is odd.";
+
+            bool isEven = num % 2 == 0;
+            if (isEven)
+            {
+                return even;
+            }
+            else
+            {
+                return odd;
+            }
+        }
+    }
+}
---
+++ CallingMethodsAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get user number to operate on
+            Console.WriteLine("What number would you like to do math operations on: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            // Call each method
+            Console.Write("Your number doubled is: " + Operation.DoubleNum(userNum));
+            Console.Write("\nYour number tripled is: " + Operation.TripleNum(userNum));
+            Console.Write("\n" + Operation.OddEven(userNum));
+
+            Console.ReadLine();
+        }
+    }
+}
