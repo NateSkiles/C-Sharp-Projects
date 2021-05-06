5cbf5f0b33eff99b6b44c61db3ac91f588ff505b
Nathan Skiles <nateskiles94@gmail.com>
2021-05-06 11:49:20 -0700

    MainMethod Assignment

---
+++ MainMethodAssignment/Operation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Operation
+    {
+        // Create a class. In that class, create a method that will take in an integer, create a math
+        // operation for this integer(addition, subtraction, etc.), then return the answer as an integer.
+        public static int Addition(int inputInt)
+        {
+            int outputInt = inputInt + 5;
+            return outputInt;
+        }
+        
+        // Add a second method to the class with the same name that will take in a decimal,
+        // create a different math operation for it, then return the answer as an integer.
+        public static decimal Addition(decimal inputDcml)
+        {
+            int outputInt = Decimal.ToInt32(inputDcml + 10m);
+            return outputInt;
+        }
+
+        // Add a third method to the class, with the same name, that will take in a string,
+        // convert it to an integer if possible, do a different math operation to it,
+        // then return the answer as an integer.
+        public static string Addition(string inputStr)
+        {
+            int inputInt = Convert.ToInt32(inputStr);
+            int outputInt = inputInt * 3;
+
+            return outputInt;
+        }
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method of the console app, instantiate the class and call the one method,
+            //passing in an integer.Display the result to the screen.
+            int input = 14;
+            int operated = Operation.Addition(input);
+            Console.WriteLine("Our input {0} when operated on with our addition method equals {1}.", input, operated);
+            Console.WriteLine("\t.addition(int) added 5 to our input.");
+
+            // In the Main() method of the console app, instantiate the class and call the second method,
+            // passing in a decimal. Display the result to the screen.
+            decimal input2 = 16.05m;
+            int operated2 = (int)Operation.Addition(input2);
+
+            // In the Main() method of the console app, instantiate the class and call the third method,
+            // passing in a string that equates to an integer. Display the result to the screen.
+            string input3 = "4";
+            int operated3 = Convert.ToInt32(Operation.Addition(input3));
+
+            Console.ReadLine();
+        }
+    }
+}
