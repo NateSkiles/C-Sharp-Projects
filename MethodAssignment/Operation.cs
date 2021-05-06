feae015ddc54cba508dbcc80b8efecea93d06e54
Nathan Skiles <nateskiles94@gmail.com>
2021-05-06 13:12:42 -0700

    Methods

---
+++ MethodAssignment/Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Operation
+    {
+        // Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. Have the method do a math operation and return an integer result.
+        public static int Multiply(int int1, int int2 = 1)
+        {
+            int calculated = int1 * int2;
+            return calculated;
+        }
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In the Main() method of the console app, instantiate the class. Ask the user to input two numbers, one at a time.
+            // Let them know they need not enter anything for the second number.
+            Console.WriteLine("Please enter up to two integers. (Note: If left blank, number two will default to (1).");
+
+            try 
+            {
+                Console.Write("Number One: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Number Two (optional): ");
+                string num2 = Console.ReadLine();
+
+                // If num2 was left blank, only num1 to method. This uses the optional parameter already set to 1
+                if (num2 == "")
+                {
+                    int multiplied = Operation.Multiply(num1);
+                    Console.WriteLine(multiplied);
+                }
+                // If num2 wasn't left blank, num2 with method.
+                else
+                {
+                    int multiplied = Operation.Multiply(num1, Convert.ToInt32(num2));
+                    Console.WriteLine(multiplied);
+                }
+            }
+            // Catch any exception errors
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
