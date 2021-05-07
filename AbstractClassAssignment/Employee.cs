3ad5fee57746b644ebbc6e13350a69eff588972c
Nathan Skiles <nateskiles94@gmail.com>
2021-05-06 17:30:07 -0700

    AbstractClassAssignment

---
+++ AbstractClassAssignment/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    public class Employee : Person
+    {
+        // Create another class called Employee and have it inherit from the Person class.
+        public override void SayName()
+        {
+            // Implement the SayName() method inside of the Employee class.
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ AbstractClassAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”.
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+            // Call the SayName() method on the object.
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
