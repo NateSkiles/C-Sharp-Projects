8e8425cea37ec09d15fc9c0973b7dccc4dbd8fa7
Nathan Skiles <nateskiles94@gmail.com>
2021-05-07 13:35:59 -0700

    update BlackJack

--- BlackJackGame/TwentyOneGame.cs
+++ BlackJackGame/TwentyOneGame.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlackJackGame
 {
-    public class TwentyOneGame : Game
+    public class TwentyOneGame : Game, IWalkAway
     {
         public override void Play()
         {
@@ -16,6 +16,10 @@ namespace BlackJackGame
         {
             Console.WriteLine("21 Players: ");
             base.ListPlayers();
+        }
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException;
         }
     }
 }
