f3d44037457263bf6e8faf6cb6bce374e0a97c85
Nathan Skiles <nateskiles94@gmail.com>
2021-05-17 18:51:45 -0700

    update

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
+using System.Collections.Generic;
 using Casino;
 using Casino.TwentyOne;
 
@@ -11,6 +14,19 @@ namespace TwentyOne
         {
             Console.WriteLine("Welcome to Nate's Casino! Let's start by telling me your name.");
             string playerName = Console.ReadLine();
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadOnlyException();
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + " ");
+                    Console.Write(exception.ExceptionType + " | ");
+                    Console.Write(exception.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
+                Console.Read();
+                return;
+            }
 
             bool validAnswer = false;
             int bank = 0;
@@ -43,9 +59,12 @@ namespace TwentyOne
                     {
                         game.Play();
                     }
-                    catch (ArgumentException)
+                    catch (FraudException ex)
                     {
-                        Console.WriteLine("Something you entered was incorrect");
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithExceptions(ex);
+                        Console.ReadLine();
+                        return;
                     }
                     catch (Exception)
                     {
@@ -59,6 +78,60 @@ namespace TwentyOne
             Console.WriteLine("Feel free to look around the casino. -- Bye for now.");
 
             Console.Read();
+        }
+
+        private static void UpdateDbWithExceptions(Exception ex)
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = TwentyOneGame; Integrated Security = True; 
+                                        Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; 
+                                        MultiSubnetFailover = False";
+
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = TwentyOneGame; Integrated Security = True; 
+                                        Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; 
+                                        MultiSubnetFailover = False";
+
+            string queryString = @"Select Id, ExceptionType, ExceptionMessage, TimeStamp From Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceeptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
         }
     }
 }
--- Casino/FraudException.cs
+++ Casino/FraudException.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Casino
 {
-    class FraudException : Exception
+    public class FraudException : Exception
     {
         public FraudException()
             :base() { }
